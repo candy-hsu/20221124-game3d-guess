3cf68e05c5c220a3632e58fe3165011a94d346ac
candy-hsu <62374659+candy-hsu@users.noreply.github.com>
2022-12-01 13:37:16 +0800

    20221201-1

---
+++ 20221124-game3d-guess/Assets/Scripts/DialogueData.cs
@@ -0,0 +1,16 @@
+﻿
+using UnityEngine;
+namespace Uzai
+{
+    /// <summary>
+    /// 對話資料
+    /// </summary>
+    [CreateAssetMenu(menuName = "Uzai/Dialogue Data", fileName = "New Dialogue Data")]
+    public class DialogueData : ScriptableObject
+    {
+
+    }
+}
+
+
+
