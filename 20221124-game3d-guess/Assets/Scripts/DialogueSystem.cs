3d221850b2d3543d1e4ac353e0e15f9f093af236
candy-hsu <62374659+candy-hsu@users.noreply.github.com>
2022-12-01 15:54:17 +0800

    20221201-7

--- 20221124-game3d-guess/Assets/Scripts/DialogueSystem.cs
+++ 20221124-game3d-guess/Assets/Scripts/DialogueSystem.cs
@@ -1,6 +1,5 @@
 
 using UnityEngine;
-using UnityEngine.UI;
 using TMPro;
 namespace Uzai
 {
@@ -9,15 +8,29 @@ namespace Uzai
     /// </summary>
     public class DialogueSystem : MonoBehaviour
     {
+        #region 對話系統
         [SerializeField, Header("對話間隔"), Range(0, 0.5f)]
         private float dialogueIntervalTime = 0.1f;
+        [SerializeField, Header("開頭對話")]
+        private DialogueData dialogueOpening;
 
-        private WaitForSeconds dialogueInterval;
+        private WaitForSeconds dialogueInterval => new WaitForSeconds(dialogueIntervalTime);
         private CanvasGroup groupDialogue;
-        private TextMeshPro textName;
-        private TextMeshPro textContent;
+        private TextMeshProUGUI textName;
+        private TextMeshProUGUI textContent;
         private GameObject goTriangle;
+        #endregion
 
+        #region 事件
+        private void Awake()
+        {
+            groupDialogue = GameObject.Find("畫布對話系統").GetComponent<CanvasGroup>();
+            textName = GameObject.Find("對話名稱").GetComponent<TextMeshProUGUI>();
+            textContent = GameObject.Find("對話內容").GetComponent<TextMeshProUGUI>();
+            goTriangle = GameObject.Find("對話完成圖示");
+            goTriangle.SetActive(false);
+        } 
+        #endregion
     }
 
 }
