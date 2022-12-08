49dda42b09a1fea86edae2bac1c7fe6bea538390
candy-hsu <62374659+candy-hsu@users.noreply.github.com>
2022-12-08 14:13:42 +0800

    20221208-2

--- 20221124-game3d-guess/Assets/Scripts/LearnCoroutine.cs
+++ 20221124-game3d-guess/Assets/Scripts/LearnCoroutine.cs
@@ -14,9 +14,15 @@ namespace Uzai
         //3.方法內必須使用 yield return (等待)
         //4.使用 StartCoroutine 啟動
 
+        private string testDialogue = "這裡好恐怖，我想要快點離開......";
+        
         private void Awake()
         {
             StartCoroutine(Test());
+
+            print("取得測試對話的第一個字：" + testDialogue[0]);
+
+            StartCoroutine(ShowDialogue());
         }
 
         private IEnumerator Test()
@@ -26,6 +32,17 @@ namespace Uzai
             print("<color=#ff3333>第二行程式</color>");
             yield return new WaitForSeconds(3);
             print("<color=#3333ff>第二行程式</color>");
+        }
+
+        private IEnumerator ShowDialogue()
+        {
+            print(testDialogue[0]);
+            yield return new WaitForSeconds(0.1f);
+            print(testDialogue[1]);
+            yield return new WaitForSeconds(0.1f);
+            print(testDialogue[2]);
+            yield return new WaitForSeconds(0.1f);
+
         }
     }
 
