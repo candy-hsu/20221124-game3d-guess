e63b81fc3a32cb5c35878ab060997fe9f82efe98
candy-hsu <62374659+candy-hsu@users.noreply.github.com>
2022-12-15 15:42:57 +0800

    20221215-6

---
+++ 20221124-game3d-guess/Assets/Scripts/InteractableSystem.cs
@@ -0,0 +1,29 @@
+﻿
+using UnityEngine;
+namespace Uzai
+{
+    /// <summary>
+    /// 互動系統：偵測玩家是否進入並且執行互動行為
+    /// </summary>
+    public class InteractableSystem : MonoBehaviour
+    {
+        //3D物件適用
+        //兩個碰撞物件必須其中一個勾選 Is Trigger
+        //碰撞開始
+        private void OnTriggerEnter(Collider other)
+        {
+            print(other.name);   
+        }
+        //碰撞結束
+        private void OnTriggerExit(Collider other)
+        {
+            
+        }
+        //碰撞持續
+        private void OnTriggerStay(Collider other)
+        {
+            
+        }
+    }
+}
+
